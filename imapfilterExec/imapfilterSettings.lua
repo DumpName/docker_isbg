7dd1be9d29551b9f886c7e9181e831fbcd32ebb2
christian.hillebrand <christian.hillebrand@media-ecom.net>
2022-07-28 21:31:14 +0200

    new ENV variables: - Set Batch sizes for, ham&spam learning and filtering individually - set max mail size in bytes - option to enable/disable verbose logging

--- imapfilterExec/imapfilterSettings.lua
+++ imapfilterExec/imapfilterSettings.lua
@@ -2,10 +2,5 @@ local settings = {}
 
 settings.configFolder = "/var/lib/mailaccounts/"
 settings.isbgPath = "isbg"
-settings.default = {}
-settings.default.batchSize = {}
-settings.default.batchSize.ham = 50
-settings.default.batchSize.spam = 50
-settings.default.batchSize.filter = 50
 
 return settings
